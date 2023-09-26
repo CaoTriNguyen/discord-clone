44220dd58238737cc4b20c76333759da336b1dcc
CaoTriNguyen <caotri.0696@gmail.com>
2023-09-26 10:24:42 +0700

    initial-modal part 2

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,4 +1,4 @@
-"use Client"
+"use client"
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,6 +12,17 @@ import {
     DialogTitle,
     DialogTrigger
 } from "@/components/ui/dialog";
+import {
+Form,
+FormControl,
+FormField,
+FormItem,
+FormLabel,
+FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 const formSchema = z.object({
     name: z.string().min(1,{
@@ -23,6 +34,11 @@ const formSchema = z.object({
 });
 
 export const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    },[]);
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -34,8 +50,12 @@ export const InitialModal = () => {
 
     const isLoading = form.formState.isSubmitting;
 
-    const onSumit = async (values: z.infer<typeof formSchema>) => {
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
+    }
+
+    if(!isMounted){
+        return null;
     }
 
     return (
@@ -49,6 +69,51 @@ export const InitialModal = () => {
                         Give your server a personality with a name and a image. You can always change it later.
                     </DialogDescription>
                 </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                TODO: Image Upload
+                            </div>
+
+                            <FormField 
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel
+                                            className="uppercase 
+                                            text-xs 
+                                            font-bold 
+                                            text-zinc-500
+                                            dark:text-secondary/70"
+                                        >
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input 
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 
+                                                border-0 
+                                                focus-visible:ring-0
+                                                text-black 
+                                                focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button variant="primary" disabled={isLoading}>
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     )
