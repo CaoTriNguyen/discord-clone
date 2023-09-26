cceda838fc18701b9a3cf6bbfb25c563e1ecf15f
CaoTriNguyen <caotri.0696@gmail.com>
2023-09-26 17:50:37 +0700

    inital modal part 3

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 
+import { FileUpload } from "../file-upload";
+
 const formSchema = z.object({
     name: z.string().min(1,{
         message: "Server name is required."
@@ -73,7 +75,19 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField 
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload 
+                                                    // endpoint="serverImage"
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />                                
                             </div>
 
                             <FormField 
